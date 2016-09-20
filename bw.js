b7cc4fdb47d6cd907c3304c726fcf416f69bab85
suckit1980 <danielrhoades87@gmail.com>
2016-09-20 09:10:12 -0700

    chap.4proj

--- bw.js
+++ bw.js
@@ -4,11 +4,49 @@
  *filename bw.js
  */
 
- var totalCost = 0;
- var canCost = 0;
+var totalCost = 0;
+var canCost = 0;
+var message = document.getElementById("message");
  
+function validateNumCans (){
+	var numbCans = document.getElementById("numcans").value;
+	var messageText = "";
+	try {
+		if (numbCans.value < 1) 
+			throw "Please enter a number greater than 0.";
+	}
+	catch(message) {
+		messageText = message;
+		numbCans.value = "";
+	}
+	finally {
+		message.innerHTML = messageText;
+		document.getElementById("numcans").value = 1;
+	}	
+}
 
- function calcCanAmount() {
+function validateYears (){
+	var numbYears = document.getElementById("years").value;
+	var messageText = "";
+	try {
+		if (numbYears.value < 1) 
+			throw "Please enter a number greater than 0 and less than 52.";
+	}
+	catch(message) {
+		messageText = message;
+		numbYears.value = "";
+	}
+	finally {
+		message.innerHTML = messageText;
+		document.getElementById("years").value = 1;
+	}	
+}
+
+/* I tried to debug this program but was unsuccesful. At first I tried using
+break points to isolate the problem but could not find it. Next I used 
+console.log but was unable to find when inputs where being processed.*/
+
+function calcCanAmount() {
 	var num = document.getElementById("numcans");
 	var yrs = document.getElementById("years");
 	totalCost -= canCost;
@@ -17,7 +55,6 @@
 	document.getElementById("totalcost").innerHTML = "$" + totalCost;
 	
 }
-
 
 function resetForm() {
 	document.getElementById("numcans").value = 1;
