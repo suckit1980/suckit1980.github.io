fd9ccefff338f250777ea039b13e5321051be822
suckit1980 <danielrhoades87@gmail.com>
2017-03-21 11:45:40 -0700

    project1

--- ad.js
+++
@@ -1,6 +0,0 @@
-console.log("Web browser name: " + navigator.appName);
-console.log("Web browser version: " + navigator.appVersion);
-console.log("User location: " + navigator.geoLoction);
-console.log("Network connection: " + navigator.onLine);
-console.log("Operating platform: " + navigator.platform);
-console.log("User agent: " + navigator.userAgent);
